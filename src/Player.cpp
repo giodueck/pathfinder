15e4482ca6dadfcc6b9ded00259501fa350feff4
giodueck <giodueck@gmail.com>
2021-12-03 19:26:05 -0300

    Added movement code to Player class

--- src/Player.cpp
+++ src/Player.cpp
@@ -8,8 +8,8 @@ Player::Player()
     {
         for (int j = 0; j < 13; j++)
         {
-            mazeGrid[i][j] = 0;
-            trackingGrid[i][j] = 0;
+            mazeGrid[i][j] = BLANK;
+            trackingGrid[i][j] = UNKNOWN;
         }
     }
 }
@@ -46,7 +46,7 @@ void Player::UnsetObjective()
         {
             if (mazeGrid[i_][j_] == OBJECTIVE)
             {
-                mazeGrid[i_][j_] = 0;
+                mazeGrid[i_][j_] = BLANK;
                 break;
             }
         }
@@ -56,7 +56,7 @@ void Player::UnsetObjective()
 void Player::ToggleObjective(int i, int j)
 {
     if (mazeGrid[i][j] == OBJECTIVE)
-        mazeGrid[i][j] = 0;
+        mazeGrid[i][j] = BLANK;
     else
         SetObjective(i, j);
 }
@@ -95,7 +95,7 @@ void Player::ToggleWall(int i, int j)
 {
     if (mazeGrid[i][j])
     {
-        mazeGrid[i][j] = 0;
+        mazeGrid[i][j] = BLANK;
         mazeWallCount--;
     }
     else if (mazeWallCount < maxWalls)
@@ -103,4 +103,253 @@ void Player::ToggleWall(int i, int j)
         mazeGrid[i][j] = WALL;
         mazeWallCount++;
     }
+}
+
+bool Player::EnterOpponent(int i)
+{
+    if (i % 2)
+    {
+        if (mazeGrid[i][0] != WALL)
+        {
+            opponentOnBoard = true;
+            return true;
+        }
+        else
+            return false;
+    }
+    else
+        throw "Invalid row, has to be uneven";
+}
+
+bool Player::Enter(Player opponent, int i)
+{
+    if (i % 2)
+    {
+        if (trackingGrid[i][0] != WALL)
+        {
+            if (opponent.EnterOpponent(i))
+            {
+                trackingGrid[i][0] = BLANK;
+                trackingGrid[i][1] = PAWN;
+                onBoard = true;
+                return true;
+            }
+            else
+            {
+                trackingGrid[i][0] = WALL;
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+        throw "Invalid row, has to be uneven";
+}
+
+void Player::GetOpponentPawn(int& i_, int& j_)
+{
+    for (int i = 1; i < 13; i += 2)
+    {
+        for (int j = 1; j < 13; j += 2)
+        {
+            if (mazeGrid[i][j] == PAWN)
+            {
+                i_ = i;
+                j_ = j;
+                return;
+            }
+        }
+    }
+
+    i_ = 0;
+    j_ = 0;
+}
+
+void Player::GetPawn(int& i_, int& j_)
+{
+    for (int i = 1; i < 13; i += 2)
+    {
+        for (int j = 1; j < 13; j += 2)
+        {
+            if (trackingGrid[i][j] == PAWN)
+            {
+                i_ = i;
+                j_ = j;
+                return;
+            }
+        }
+    }
+
+    i_ = 0;
+    j_ = 0;
+}
+
+bool Player::MoveOpponent(int direction)
+{
+    if (opponentOnBoard)
+    {
+        int i, j;
+        GetOpponentPawn(i, j);
+
+        switch (direction)
+        {
+        case UP:
+            if (mazeGrid[i - 1][j] == WALL)
+                return false;
+            else if (i - 2 > 0)
+            {
+                mazeGrid[i - 2][j] = PAWN;
+                mazeGrid[i][j] = VISITED;
+            }
+            return true;
+            break;
+        case DOWN:
+            if (mazeGrid[i + 1][j] == WALL)
+                return false;
+            else if (i + 2 < 13)
+            {
+                mazeGrid[i + 2][j] = PAWN;
+                mazeGrid[i][j] = VISITED;
+            }
+            return true;
+            break;
+        case LEFT:
+            if (mazeGrid[i][j - 1] == WALL)
+                return false;
+            else if (j - 2 > 0)
+            {
+                mazeGrid[i][j - 2] = PAWN;
+                mazeGrid[i][j] = VISITED;
+            }
+            return true;
+            break;
+        case RIGHT:
+            if (mazeGrid[i][j + 1] == WALL)
+                return false;
+            else if (j + 2 < 13)
+            {
+                mazeGrid[i][j + 2] = PAWN;
+                mazeGrid[i][j] = VISITED;
+            }
+            return true;
+            break;
+
+        default:
+            throw "Invalid direction";
+        }
+    }
+    else
+        throw "Invalid action, pawn not on board";
+}
+
+bool Player::Move(Player opponent, int direction)
+{
+    if (onBoard)
+    {
+        int i, j;
+        GetPawn(i, j);
+
+        switch (direction)
+        {
+        case UP:
+            if (opponent.MoveOpponent(direction))
+            {
+                if (i - 2 > 0)
+                {
+                    trackingGrid[i - 2][j] = PAWN;
+                    trackingGrid[i][j] = VISITED;
+                }
+                return true;
+            }
+            else
+            {
+                SetTrackingWall(i - 1, j);
+                return false;
+            }
+            break;
+        case DOWN:
+            if (opponent.MoveOpponent(direction))
+            {
+                if (i + 2 < 13)
+                {
+                    trackingGrid[i + 2][j] = PAWN;
+                    trackingGrid[i][j] = VISITED;
+                }
+                return true;
+            }
+            else
+            {
+                SetTrackingWall(i + 1, j);
+                return false;
+            }
+            break;
+        case LEFT:
+            if (opponent.MoveOpponent(direction))
+            {
+                if (j - 2 > 0)
+                {
+                    trackingGrid[i][j - 2] = PAWN;
+                    trackingGrid[i][j] = VISITED;
+                }
+                return true;
+            }
+            else
+            {
+                SetTrackingWall(i, j - 1);
+                return false;
+            }
+            break;
+        case RIGHT:
+            if (opponent.MoveOpponent(direction))
+            {
+                if (j + 2 < 13)
+                {
+                    trackingGrid[i][j + 2] = PAWN;
+                    trackingGrid[i][j] = VISITED;
+                }
+                return true;
+            }
+            else
+            {
+                SetTrackingWall(i, j + 1);
+                return false;
+            }
+            break;
+
+        default:
+            throw "Invalid direction";
+        }
+    }
+    else
+        throw "Invalid action, pawn not on board";
+}
+
+void Player::RetreatOpponent()
+{
+    for (int i = 0; i < 13; i++)
+    {
+        for (int j = 0; j < 13; j++)
+        {
+            if (mazeGrid[i][j] == PAWN)
+            {
+                mazeGrid[i][j] = BLANK;
+                break;
+            }
+        }
+    }
+    opponentOnBoard = false;
+}
+
+void Player::Retreat(Player opponent)
+{
+    opponent.RetreatOpponent();
+    onBoard = false;
+}
+
+bool Player::PawnOnBoard()
+{
+    return onBoard;
 }
--- src/Player.h
+++ src/Player.h
@@ -2,10 +2,20 @@
 
 enum Pieces
 {
+    UNKNOWN = -1,   // for tracking grid
+    BLANK = 0,
     OBJECTIVE = 1,
     PAWN = 2,
     VISITED = 3,
     WALL = 4
+};
+
+enum Direction
+{
+    UP = 0,
+    LEFT = 1,
+    DOWN = 2,
+    RIGHT = 3
 };
 
 class Player
@@ -32,14 +42,32 @@ public:
 
     int GetTrackingWallCount();
 
+    bool PawnOnBoard();
+
     void ToggleWall(int i, int j);
 
     void UnsetObjective();
 
     void ToggleObjective(int i, int j);
 
+    bool EnterOpponent(int i);
+
+    bool Enter(Player opponent, int i);
+
+    bool MoveOpponent(int direction);
+
+    bool Move(Player opponent, int direction);
+
+    void RetreatOpponent();
+
+    void Retreat(Player opponent);
+
 protected:
     int mazeGrid[13][13];
     int trackingGrid[13][13];
     int mazeWallCount, trackingWallCount;
+    bool onBoard = false, opponentOnBoard = false;
+
+    void GetOpponentPawn(int& i, int& j);
+    void GetPawn(int& i, int& j);
 };
